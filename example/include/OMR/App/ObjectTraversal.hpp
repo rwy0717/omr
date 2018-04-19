cf445f9b38c7c93c3ff8d4cc429e7f200b2aac3c
Robert Young <rwy0717@gmail.com>
2018-04-19 19:00:53 -0400

    Prototype object walk in marking scheme
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- example/include/OMR/App/ObjectTraversal.hpp
+++ example/include/OMR/App/ObjectTraversal.hpp
@@ -1,4 +1,23 @@
-
+/*******************************************************************************
+ * Copyright (c) 2018, 2018 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
 
 #if !defined(OMR_APP_OBJECTTRAVERSAL_HPP_)
 #define OMR_APP_OBJECTTRAVERSAL_HPP_
@@ -20,7 +39,7 @@ public:
 	 * Visit each slot of object with visitor.
 	 */
 	template <class VisitorT>
-	size_t visit(Object* object, VisitorT& visitor, size_t sizeToDo = SIZE_MAX)
+	static void visit(Object* object, VisitorT& visitor, size_t sizeToDo = SIZE_MAX)
 	{
 		for (Slot* slot = object->begin(); slot != object->end(); ++slot) {
 			visitor.edge(object, SlotHandle(slot));
@@ -28,7 +47,7 @@ public:
 	}
 };
 
-}  // namespace App
-}  // namespace OMR
+}  /* namespace App */
+}  /* namespace OMR */
 
-#endif  // OMR_APP_OBJECTTRAVERSAL_HPP_
+#endif  /* OMR_APP_OBJECTTRAVERSAL_HPP_ */
