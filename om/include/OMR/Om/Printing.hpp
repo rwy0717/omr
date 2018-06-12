1c89c3fe70720602ae0dad34b8e8691a7063ef94
Robert Young <rwy0717@gmail.com>
2018-06-12 10:43:02 -0400

    Fix up some printing routines
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- om/include/OMR/Om/Printing.hpp
+++ om/include/OMR/Om/Printing.hpp
@@ -3,6 +3,9 @@
 
 #include <OMR/Om/BasicSlotHandle.hpp>
 #include <OMR/Om/Cell.hpp>
+#include <OMR/Om/Shape.hpp>
+#include <OMR/Om/SlotAttr.hpp>
+#include <OMR/Om/SlotDescriptor.hpp>
 #include <OMR/Om/Value.hpp>
 #include <OMR/Om/ValueSlotHandle.hpp>
 
@@ -10,6 +13,21 @@
 
 namespace OMR {
 namespace Om {
+
+inline std::ostream& operator<<(std::ostream& out, Id id) {
+	return out << "(id " << id.raw() << ")";
+}
+
+inline std::ostream& operator<<(std::ostream& out, CoreType type) {
+	switch (type) {
+	case CoreType::FLOAT32: return out << "float32";
+	case CoreType::FLOAT64: return out << "float64";
+	case CoreType::INT8: return out << "int8";
+	case CoreType::INT16: return out << "int16";
+	// TODO: print all the core types.
+	default: return out << "unknown";
+	}
+}
 
 inline std::ostream& operator<<(std::ostream& out, Value value) {
 	switch (value.kind()) {
@@ -22,17 +40,24 @@ inline std::ostream& operator<<(std::ostream& out, Value value) {
 	}
 }
 
-inline std::ostream& operator<<(std::ostream& out, const SlotAttr)
-
-        inline std::ostream&
-        operator<<(std::ostream& out, const SlotDescriptor& sd) {
-	return out "(slot-descriptor " << sd.offset() << " " << sd.attr() << ")";
+inline std::ostream& operator<<(std::ostream& out, const SlotType& type) {
+	return out << "(slot-type " << type.coreType() << " " << type.id() << ")";
 }
+
+inline std::ostream& operator<<(std::ostream& out, const SlotAttr& attr) {
+	return out << "(slot-attr " << attr.type() << attr.id() << ")";
+}
+
+inline std::ostream& operator<<(std::ostream& out, const SlotDescriptor& sd) {
+	return out << "(slot-descriptor " << sd.offset() << " " << sd.attr() << ")";
+}
+
 inline std::ostream& operator<<(std::ostream& out, const Shape& shape) {
-	out << "(shape" for (const SlotAttr* iter = shape->instanceSlotAttr().begin();
-	                     it != shape->instanceSlotAttrs().end();
-	                     it++) {
-		out " (slot: " << *slot << ")";
+	out << "(shape ";
+	for (const SlotAttr* iter = shape.instanceSlotAttrs().begin();
+	     iter != shape.instanceSlotAttrs().end();
+	     ++iter) {
+		out << "\n  (slot: " << *iter << ")";
 	}
 	return out << ")";
 }
