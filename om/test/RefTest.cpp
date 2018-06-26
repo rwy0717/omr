38dadf7cc408e1d602b1d05ec39160a1f5b55f4a
Robert Young <rwy0717@gmail.com>
2018-06-26 10:24:04 -0400

    Add some more Ref tests
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- om/test/RefTest.cpp
+++ om/test/RefTest.cpp
@@ -11,6 +11,7 @@ TEST(RefTest, nullVoidReferent) {
 	EXPECT_EQ(ref, nullptr);
 	EXPECT_EQ(ref, Ref<int>(nullptr));
 	EXPECT_EQ(ref, ref);
+	EXPECT_FALSE(ref);
 }
 
 TEST(RefTest, nullIntReferent) {
@@ -18,21 +19,63 @@ TEST(RefTest, nullIntReferent) {
 	EXPECT_EQ(ref, nullptr);
 	EXPECT_EQ(ref, Ref<void>(nullptr));
 	EXPECT_EQ(ref, ref);
+	EXPECT_FALSE(ref);
 }
 
 TEST(RefTest, intReferent) {
-	int referent = 99;
-	Ref<int> ref = &referent;
+	int a = 99;
+	int b = 88;
 
-	EXPECT_EQ(ref, &referent);
-	EXPECT_EQ(*ref, 99);
-	EXPECT_NE(ref, nullptr);
-	EXPECT_EQ(ref, ref);
-	EXPECT_EQ(ref.get(), &referent);
+	Ref<int> ra = &a;
+	Ref<int> rb = &b;
 
-	int referent2;
-	auto ref2 = makeRef(&referent2);
-	EXPECT_NE(ref, ref2);
+	EXPECT_EQ(ra, &a);
+	EXPECT_EQ(*ra, 99);
+	EXPECT_NE(ra, nullptr);
+	EXPECT_EQ(ra, ra);
+	EXPECT_EQ(ra.get(), &a);
+	EXPECT_EQ(ra, Ref<void>(&a));
+
+	EXPECT_NE(ra, rb);
+	EXPECT_EQ(*rb, b);
+	EXPECT_NE(*ra, *rb);
+}
+
+class A {};
+
+class B : public A {};
+
+bool check(const Ref<A>& a) { return true; }
+
+TEST(RefTest, referentInheritsFromType) {
+	A a;
+	B b;
+
+	{
+		Ref<A> ra = &a;
+		Ref<B> rb = &b;
+		EXPECT_NE(ra, rb);
+		EXPECT_NE(rb, ra);
+	}
+
+	{
+		Ref<A> ra = &a;
+		Ref<A> rb = &b;
+		EXPECT_NE(ra, rb);
+		EXPECT_NE(rb, ra);
+	}
+
+	{
+		Ref<A> rb1 = &b;
+		Ref<B> rb2 = &b;
+		EXPECT_EQ(rb1, rb2);
+		EXPECT_EQ(rb2, rb1);
+	}
+
+	{
+		Ref<B> r = &b;
+		check(r);
+	}
 }
 
 }  // namespace Test
