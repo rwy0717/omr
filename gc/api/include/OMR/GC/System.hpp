39921ed71c65a2805ebd4cf5435ddf06f437ce54
Robert Young <rwy0717@gmail.com>
2018-08-31 11:51:14 -0400

    Add experimental GC context and object scanner APIs
    This commit introduces two experimental APIs to the GC. Both APIs are
    only supported under cmake, and are permanantly disabled in autotools.
    
    The first is a high level wrapper for initializing and tearing down the
    GC. To enable this API, use the flag OMR_GC_EXPERIMENTAL_CONTEXT.
    This code is grouped under the gc/api directory.
    
    In the experimental context APIs, the  GC is structured into three
    tiers:
    
    1. OMR::Runtime - RAII-style process-wide initialization. Creating a
       Runtime will bring  up the thread library, port library, and
       OMR_Runtime. There should only be one instance of the runtime in a
       program.
    2. OMR::GC::System - A managed heap. Provides it's own shared-access
       mechanism. Typically used on a per-VM basis.
    3. OMR::GC::Context - per-thread context attached to a GC::System. Most
       GC APIs require a GC::Context. Having a context implies shared heap
       accss. Contexts can be upgraded to exclusive contexts.
    
    The implementation of this API is placed under gc/api. New public
    headers are grouped under gc/api/include/OMR/GC/*. They are meant to be
    included as #include <OMR/GC/Context.hpp>.
    
    GC::System  implements it's own shared access mechanism.  Implementing
    it's own heap access mechanism requires that some GC glue is actually
    provided by the GC, such as the EnvironmentLanguageInterface.
    
    To support this notion of gc-provided glue, this PR rearranges the
    example's glue headers into include and include_extended.
    include_extended is only added to the ipath if
    OMR_GC_EXPERIMENTAL_CONTEXT is off, to ensure there is only ever one
    version of each glue header available.
    
    The second API is a new piece of glue, the OMRClient::GC::ObjectScanner.
    This is an attempt to write an object scanner API that is simpler for
    clients to implement, while  supporting custom reference encodings. The
    new glue APIs are placed under the OMRClient namespace. Public headers
    are included as <OMRClient/GC/ObjectScanner.hpp>.
    
    The new object scanner API
    
    The experimental object scanner makes compressed object pointers a
    concern of the application. The long term goal is to remove the
    compressed references flag from OMR entirely. Through utility classes
    provided by OMR, implementing compressed references should be trivial
    for applications to implement.
    
    The object scanner, if successful, will deprecate:
    * fomrobject_t
    * GC_SlotObject
    
    Signed-off-by: Robert Young <rwy0717@gmail.com>

---
+++ gc/api/include/OMR/GC/System.hpp
@@ -0,0 +1,212 @@
+/*******************************************************************************
+ *  Copyright (c) 2018, 2018 IBM and others
+ *
+ *  This program and the accompanying materials are made available under
+ *  the terms of the Eclipse Public License 2.0 which accompanies this
+ *  distribution and is available at https://www.eclipse.org/legal/epl-2.0/
+ *  or the Apache License, Version 2.0 which accompanies this distribution and
+ *  is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *  This Source Code may also be made available under the following
+ *  Secondary Licenses when the conditions for such availability set
+ *  forth in the Eclipse Public License, v. 2.0 are satisfied: GNU
+ *  General Public License, version 2 with the GNU Classpath
+ *  Exception [1] and GNU General Public License, version 2 with the
+ *  OpenJDK Assembly Exception [2].
+ *
+ *  [1] https://www.gnu.org/software/classpath/license.html
+ *  [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ *  SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#if !defined(OMR_GC_SYSTEM_HPP_)
+#define OMR_GC_SYSTEM_HPP_
+
+#include <OMR/GC/MarkingFn.hpp>
+#include <OMR/GC/StackRootList.hpp>
+
+#include <EnvironmentBase.hpp>
+#include <OMR/Runtime.hpp>
+#include <OMR/IntrusiveList.hpp>
+#include <OMR_VMThread.hpp>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <mminitcore.h>
+#include <new>
+#include <omrutil.h>
+#include <type_traits>
+
+class MM_EnvironmentBase;
+
+namespace OMR
+{
+namespace GC
+{
+
+class Visitor;
+class BaseContext;
+class Context;
+class StartupContext;
+
+class StartupError : public ::std::runtime_error
+{
+	using runtime_error::runtime_error;
+};
+
+using ContextList = IntrusiveList<Context>;
+using ContextListNode = ContextList::Node;
+using ContextListIterator = ContextList::Iterator;
+
+/// A GC subsystem.
+class System
+{
+public:
+	explicit System(OMR::Runtime &runtime);
+
+	System(const System &) = delete;
+
+	/// TODO: Move ctor for system
+	System(System &&) = delete;
+
+	~System();
+
+	OMR_VM &vm() { return _vm; }
+
+	const OMR_VM &vm() const { return _vm; }
+
+	Runtime &runtime() { return *_runtime; }
+
+	ContextList &contexts() noexcept { return _contexts; }
+
+	const ContextList &context() const noexcept { return _contexts; }
+
+	MarkingFnVector &markingFns() noexcept { return _userRoots; }
+
+	const MarkingFnVector &markingFns() const noexcept { return _userRoots; }
+
+private:
+	friend class BaseContext;
+	friend class Context;
+	friend class StartupContext;
+
+	/// Initialize the VM, which ties together all the OMR services.
+	void initVm();
+
+	/// Initialize the heap and collector.
+	void initCollector();
+
+	void initGcSlaveThreads(StartupContext &cx);
+
+	Runtime *_runtime;
+	OMR_VM _vm;
+	ContextList _contexts;
+	MarkingFnVector _userRoots;
+};
+
+static_assert(std::is_standard_layout<System>::value,
+              "OMR::GC::System must be a standard layout type.");
+
+/// A GC context. Base class. GC users should create Contexts.
+class BaseContext
+{
+public:
+	static constexpr const char *THREAD_NAME = "OMR::GC::Context";
+
+	System &system() const noexcept { return *_system; }
+
+	MM_EnvironmentBase *env() const noexcept { return MM_EnvironmentBase::getEnvironment(vmContext()); }
+
+	OMR_VMThread *vmContext() const noexcept { return _vmContext; }
+
+protected:
+	BaseContext(System &system)
+	    : _thread(system.runtime().platform().thread()), _system(&system), _vmContext(nullptr)
+	{
+		attachVmContext(system.vm(), *this);
+	}
+
+	~BaseContext() noexcept {
+		detachVmContext(_system->vm(), *this);
+		_system = nullptr;
+		_vmContext = nullptr;
+	}
+
+private:
+	Thread _thread;
+	System *_system;
+	OMR_VMThread *_vmContext; //< TODO: Delete this when we can.
+
+	static void attachVmContext(OMR_VM &vm, BaseContext &cx);
+
+	static void detachVmContext(OMR_VM &vm, BaseContext &cx);
+};
+
+static_assert(std::is_standard_layout<System>::value,
+              "OMR::GC::BaseContext must be a standard layout type.");
+
+class Context : public BaseContext {
+public:
+	explicit inline Context(System& sys) : BaseContext(sys) {
+		system().contexts().add(this);
+	}
+
+	~Context() noexcept {
+		system().contexts().remove(this);
+	}
+
+	ContextListNode& node() noexcept { return _node; }
+
+	const ContextListNode& node() const noexcept { return _node; }
+
+	StackRootList &stackRoots() noexcept { return _stackRoots; }
+
+	const StackRootList &stackRoots() const noexcept { return _stackRoots; }
+
+	MarkingFnVector &markingFns() noexcept { return _userMarkingFns; }
+
+	const MarkingFnVector &markingFns() const noexcept { return _userMarkingFns; }
+
+private:
+	ContextListNode _node;
+	StackRootList _stackRoots;
+	MarkingFnVector _userMarkingFns;
+};
+
+// static_assert(std::is_standard_layout<Context>::value,
+//               "The GC context must be a StandardLayoutType for calculating JIT offsets.");
+
+/// A special limited context that is only used during startup or shutdown.
+class StartupContext : public BaseContext
+{
+private:
+	/// Only the GC::System can bring up a startup context.
+	friend class System;
+
+	StartupContext(System &system) : BaseContext(system) {}
+
+	StartupContext(const StartupContext &other) = delete;
+};
+
+/// @group Context conversion routines
+/// @{
+
+inline Context &
+getContext(OMR_VMThread *vmContext)
+{
+	return *static_cast<Context *>(vmContext->_language_vmthread);
+}
+
+inline Context &
+getContext(MM_EnvironmentBase *env)
+{
+	return getContext(env->getOmrVMThread());
+}
+
+/// @}
+
+} // namespace GC
+} // namespace OMR
+
+#endif // OMR_GC_SYSTEM_HPP_
