d9711f022c1b326457620cb07668bae9f34a4550
Robert Young <rwy0717@gmail.com>
2019-02-28 17:17:30 -0500

    Add unsigned int overloads for IlBuilder::Const
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- compiler/ilgen/OMRIlBuilder.hpp
+++ compiler/ilgen/OMRIlBuilder.hpp
@@ -245,6 +245,10 @@ public:
    TR::IlValue *Const(int16_t value)            { return ConstInt16(value); }
    TR::IlValue *Const(int32_t value)            { return ConstInt32(value); }
    TR::IlValue *Const(int64_t value)            { return ConstInt64(value); }
+   TR::IlValue *Const(uint8_t value)            { return ConstInt8(value); }
+   TR::IlValue *Const(uint16_t value)           { return ConstInt16(value); }
+   TR::IlValue *Const(uint32_t value)           { return ConstInt32(value); }
+   TR::IlValue *Const(uint64_t value)           { return ConstInt64(value); }
    TR::IlValue *Const(float value)              { return ConstFloat(value); }
    TR::IlValue *Const(double value)             { return ConstDouble(value); }
    TR::IlValue *Const(const void * const value) { return ConstAddress(value); }
