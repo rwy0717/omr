908493470f5832f5f41999d9b1e349638d8fba87
Robert Young <rwy0717@gmail.com>
2020-07-03 13:19:08 -0400

    Include <netinet/in.h> in omrsock.h
    This header is responsible for defining the in6_addr struct. This type
    is used in the header <netinet/tcp.h>. On some versions of AIX ,tcp.h
    does not include in.h (despite using types defined there), so we have to
    manually include it before tcp.h. This isn't a problem on other posix
    platforms.
    
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- port/unix_include/omrsock.h
+++ port/unix_include/omrsock.h
@@ -40,6 +40,7 @@
 
 #include <sys/types.h> /* Some historical implementations need this file, POSIX.1-2001 does not. */
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <netinet/tcp.h>
 
 typedef struct sockaddr omr_os_sockaddr;
